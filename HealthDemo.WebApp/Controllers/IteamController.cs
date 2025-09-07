d896677ba2723241d34477ac7e0b2b64370b50b7
bhartitomartejveer <bhartitomartejveer@outlook.com>
2025-09-07 19:46:04 +0530

    Adde multipe controller

---
+++ HealthDemo.WebApp/Controllers/IteamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthDemo.WebApp.Controllers
+{
+    public class IteamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HealthDemo.WebApp/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthDemo.WebApp.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
