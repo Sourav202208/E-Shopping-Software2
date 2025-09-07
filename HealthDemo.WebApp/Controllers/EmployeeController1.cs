0eda01f69a1a4f7e36b1d3caaf06588a6b276889
bhartitomartejveer <bhartitomartejveer@outlook.com>
2025-09-07 19:31:39 +0530

    Added EmployeeController1 and other changes

---
+++ HealthDemo.WebApp/Controllers/EmployeeController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthDemo.WebApp.Controllers
+{
+    public class EmployeeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+             string message=string.Empty;
+            return View();
+        }
+    }
+}
